b83d7c8d7a3fc37dcc92ea0e29e326b2688b5468
DevY <hanys1042@gmail.com>
2019-04-12 01:24:11 +0100

    first commit

---
+++ src/main/java/devy/lotto/data/LottoDataApplication.java
@@ -0,0 +1,13 @@
+package devy.lotto.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottoDataApplication.class, args);
+	}
+
+}
